43e61377897e3313c70a3fd519c79fe3e7952af0
smr <zyad000999000@gmail.com>
2026-01-13 05:14:00 +0300

    Update server.js

--- krb-site/server.js
+++ krb-site/server.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 0 }));
 app.use(express.json());
 
 /* =======================
